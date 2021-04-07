6abb579f9d3888d0b50a53d82a8f0b9fb6e4de7b
赵志坚 <zhijian.zhao@ruisdata.com>
2021-04-07 22:01:30 +0800

    'update：publicPath'

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   outputDir: 'dist',
-  publicPath: process.env.NODE_ENV === 'production' ? '/test-vue-create/' : '/'
+  // 配置自己域名前
+  // publicPath: process.env.NODE_ENV === 'production' ? '/test-vue-create/' : '/'
+  // 配置自己域名后
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
 }
